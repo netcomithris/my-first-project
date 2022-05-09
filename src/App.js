2e943e5519091d34f157c1b441df72271f7bcc53
netcom <netcomithrismohamed@gmail.com>
2022-05-09 12:49:53 +0530

    add paragraph

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>ithris mohamed</h1>
+      <p>Most of the stories of your childhood were probably stories with morals. These are not the kind of stories we see very often these days. Wouldn’t it be amazing to share these stories with your child? Why not start with this list that we have put together for you.</p>
     </div>
   );
 }
